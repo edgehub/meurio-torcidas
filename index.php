6aba67b60c836da3cb1667e7d34ac611a88033e3
Marlus Araujo <mar251@gmail.com>
2011-10-14 23:12:52 -0300

    no duplicates: changed cached object, created loop for checking

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $cached = $json->decode( $cache_content );
 
 $current_time = time(); 
 $expire_time = $EXPIRE_TIME * 60 * 60; 
-$file_time = filemtime($TWITTER_CACHE);
+$file_time = filemtime($TWITTER_CACHE_FILE);
 
 if(file_exists($TWITTER_CACHE_FILE) && ($current_time - $expire_time < $file_time)) {
 	//echo 'returning from cached file';
@@ -41,31 +41,47 @@ else {
 	$t = new summize();
 
 	$timeline = $t->search($flaQuery);
-	$cached->flamengo += count($timeline->results);
+	$cached->flamengo->count += getLatest($timeline->results, $cached->flamengo->last_id);
+	$cached->flamengo->last_id = $timeline->max_id_str;
 
 	$timeline = $t->search($fluQuery);
-	$cached->fluminense += count($timeline->results);
+	$cached->fluminense->count += getLatest($timeline->results, $cached->fluminense->last_id);
+	$cached->fluminense->last_id = $timeline->max_id_str;
 
 	$timeline = $t->search($vascoQuery);
-	$cached->vasco += count($timeline->results);
+	$cached->vasco->count += getLatest($timeline->results, $cached->vasco->last_id);
+	$cached->vasco->last_id = $timeline->max_id_str;
 
 	$timeline = $t->search($botaQuery);
-	$cached->botafogo += count($timeline->results);
+	$cached->botafogo->count += getLatest($timeline->results, $cached->botafogo->last_id);
+	$cached->botafogo->last_id = $timeline->max_id_str;
 	
 	file_put_contents($TWITTER_CACHE_FILE,$json->encode($cached));
 }
 
-echo "MeuRioéFla: " . $cached->flamengo . "<br/><hr />";
+echo "MeuRioéFla: " . $cached->flamengo->count . "<br/><hr />";
 
-echo "MeuRioéFlu: " . $cached->fluminense . "<br/><hr />";
+echo "MeuRioéFlu: " . $cached->fluminense->count . "<br/><hr />";
 
-echo "MeuRioéVasco: " . $cached->vasco . "<br/><hr />";
+echo "MeuRioéVasco: " . $cached->vasco->count . "<br/><hr />";
 
-echo "MeuRioéFogo: " . $cached->botafogo . "<br/><hr />";
-
-
+echo "MeuRioéFogo: " . $cached->botafogo->count . "<br/><hr />";
 
 
+$t = new summize();
+$timeline = $t->search($botaQuery);
+
+function getLatest($results, $last_id) {
+	$k = 0;
+	foreach($results as $r){
+		if($r->id_str > $last_id){
+			$k++;
+		}else{
+			return $k;
+		}
+	}
+	return $k;
+}
 
 
 //Codigo ruim
